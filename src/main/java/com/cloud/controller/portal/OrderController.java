630958b6d621e0d9d421b44a71105772dbe1296a
zx <279742310@qq.com>
2019-01-04 11:30:04 +0800

    no spring session

--- src/main/java/com/cloud/controller/portal/OrderController.java
+++ src/main/java/com/cloud/controller/portal/OrderController.java
@@ -164,7 +164,7 @@ public class OrderController {
         }
         log.info("支付宝回调,sign:{},trade_status:{},参数:{}",params.get("sign"),params.get("trade_status"),params.toString());
 
-        //非常重要,验证回调的正确性,是不是支付宝发的.并且呢还要避免重复通知.
+        //非常重要,验证回调的正确性,是不是支付宝发的.并且呢还要避免重复通知..
 
         params.remove("sign_type");
         try {
